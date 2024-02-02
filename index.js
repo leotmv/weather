81631013d0361c94b3c38d96bd011f020620725a
LEONARDO VIEIRA <leonardotmv71@gmail.com>
2024-02-02 02:05:58 -0300

    first Weather project commit

--- index.js
+++ index.js
@@ -4,25 +4,29 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3001;
-const apiKey = "ca9b56844a165bb48b4d0dadb541c592";
-let cityName = 0;
+const apiKey = "";
 let latitude = 0;
 let longitude = 0;
+let cityName = "";
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 app.get("/", (req,res)=>{
-    res.render("index.ejs");
+    res.render("index.ejs"), {
+        cityNameH1: "Welcome! Enter a city name!"
+    };
 });
 
 app.post("/submit", async (req,res)=>{
     cityName = req.body.cityName;
-    console.log(cityName);
     try{
         const mainlyUrl = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=2&appid=${apiKey}`);
         longitude = mainlyUrl.data[0].lon;
         latitude = mainlyUrl.data[0].lat;
-        res.render("index.ejs");
+        res.render("index.ejs", {
+            cityNameH1: cityName,
+            key: "yes"
+        });
     }catch(error){
         console.log(error.message);
     }
@@ -30,11 +34,12 @@ app.post("/submit", async (req,res)=>{
 
 app.get("/parameters", async (req,res)=>{
     try{
+        const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`);
         const tempMax = Math.floor(result.data.main.temp_max - 273);
         const tempMin = Math.floor(result.data.main.temp_min - 273);
-        const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`);
-        console.log(mainlyUrl.data[0].lat, mainlyUrl.data[0].lon);
         res.render("index.ejs", {
+            key: "yes",
+            cityNameH1: cityName,
             willItRain: result.data.weather[0].main,
             intensity: result.data.weather[0].description,
             tempMax: tempMax,
